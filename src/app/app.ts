28f90394f44a9bacf42d2d2f172c4151ca5525c7
Harsimran Virk <harsimranvirk99@gmail.com>
2022-06-05 20:52:23 +0530

    Replaced input number with range

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,6 +8,11 @@ Logger.log("Binding inputs");
 const inputEl = document.getElementById("inputEl") as HTMLInputElement;
 const retryButton = document.getElementById("retryButton") as HTMLDivElement;
 const chessBoardEl = document.getElementById("chessboard") as HTMLDivElement;
+const outputLabel = document.getElementById("output") as HTMLLabelElement;
+
+inputEl.addEventListener('change', (e) => {
+  outputLabel.innerText = (e.target as HTMLInputElement)?.value;
+})
 
 const chessBoard = new Chessboard(chessBoardEl);
 const inputListener = new InputListener(chessBoard, inputEl, retryButton);
